7c7f229c26a53c8f1aa0bb67627f1944bf8edbe3
Average-Joe18 <60050146+Average-Joe18@users.noreply.github.com>
2023-04-15 19:46:07 -0700

    crimeByDateByDep Prototype 2.0

--- datathon/crimeByDateByDep.py
+++ datathon/crimeByDateByDep.py
@@ -24,20 +24,22 @@ df['MONTHYEAR'] = df['DATE'].apply(getMonthYear)
 maps = []
 
 for monthyear in df.MONTHYEAR.unique():
-
     df2 = df.drop(df[df.MONTHYEAR != monthyear].index)
     crime_freq = df2.dropna(subset=['PDQ'])
     crime_freq = crime_freq.groupby('PDQ').agg({'CATEGORIE': 'count'}).sort_values(by='CATEGORIE',ascending=False)
     crime_freq = crime_freq.rename(columns={'CATEGORIE': 'Crime Count'})
     combined_table = pd.merge(df, crime_freq, on="PDQ", how='inner')
     combined_table = combined_table.drop_duplicates(subset=['PDQ']).sort_values(by='PDQ', ascending=False)
+
     fig = px.choropleth_mapbox(combined_table, geojson=geojson, 
-                            color = "Crime Count", # Might need to change depending on what you want to measure
-                            locations="PDQ", featureidkey="properties.PDQ",
-                            center={'lat': 45.508888, 'lon': -73.561668}, # Can change the coordinates to make city more centered
-                            mapbox_style="carto-positron", # Another option is called "open-street-map" :o
-                            zoom=9
-                            )
+        color = "Crime Count",
+        locations="PDQ", featureidkey="properties.PDQ",
+        center={'lat': 45.508888, 'lon': -73.561668},
+        mapbox_style="carto-positron",
+        zoom=9,
+        color_continuous_scale='Viridis',
+        range_color=[0, 250]
+    )
 
     maps.append(fig)
 
@@ -58,7 +60,7 @@ app.layout = html.Div(
         dcc.Slider(min=0,
                    max=98,
                    step=1,
-                   value=99,
+                   value=98,
                    marks=rangeslider_marks,
                    tooltip={"placement": "bottom", "always_visible": False},
                    updatemode='drag',
