80ff25faab2d333e8211b315c973b078c5d8327e
Average-Joe18 <60050146+Average-Joe18@users.noreply.github.com>
2023-04-15 19:06:37 -0700

    Interactive Date Map Prototype 1.0

--- datathon/test.py
+++ datathon/test.py
@@ -13,6 +13,39 @@ geojson = json.load(open(geojsonPath, "r"))
 criminalLogsPath = Path.cwd() / 'actes-criminels.csv'
 df = pd.read_csv(criminalLogsPath)
 
+#Add the extra column Monthyear to df
+def getMonthYear(str):
+    return str.split('-')[0] + '-' + str.split('-')[1]
+df['MONTHYEAR'] = df['DATE'].apply(getMonthYear)
+
+
+#CRUCIAL STUFF____________________________________________________________________________
+
+maps = []
+
+for monthyear in df.MONTHYEAR.unique():
+
+    df2 = df.drop(df[df.MONTHYEAR != monthyear].index)
+    crime_freq = df2.dropna(subset=['PDQ'])
+    crime_freq = crime_freq.groupby('PDQ').agg({'CATEGORIE': 'count'}).sort_values(by='CATEGORIE',ascending=False)
+    crime_freq = crime_freq.rename(columns={'CATEGORIE': 'Crime Count'})
+    combined_table = pd.merge(df, crime_freq, on="PDQ", how='inner')
+    combined_table = combined_table.drop_duplicates(subset=['PDQ']).sort_values(by='PDQ', ascending=False)
+    fig = px.choropleth_mapbox(combined_table, geojson=geojson, 
+                            color = "Crime Count", # Might need to change depending on what you want to measure
+                            locations="PDQ", featureidkey="properties.PDQ",
+                            center={'lat': 45.508888, 'lon': -73.561668}, # Can change the coordinates to make city more centered
+                            mapbox_style="carto-positron", # Another option is called "open-street-map" :o
+                            zoom=9
+                            )
+
+    maps.append(fig)
+
+#CRUCIAL STUFF____________________________________________________________________________
+
+
+
+
 rangeslider_marks = {0:'2015', 12:'2016', 24:'2017', 36:'2018', 48:'2019',
                      60:'2020', 72:'2021', 84:'2022', 96:'2023'}
 
@@ -46,22 +79,7 @@ app.layout = html.Div(
 )
 def update_graph(dateindex):
 
-    crime_freq = df.dropna(subset=['PDQ'])
-
-    crime_freq = crime_freq.groupby('PDQ').agg({'CATEGORIE': 'count'}).sort_values(by='CATEGORIE',ascending=False)
-    crime_freq = crime_freq.rename(columns={'CATEGORIE': 'crime_count'})
-    combined_table = pd.merge(df, crime_freq, on="PDQ", how='inner')
-    combined_table = combined_table.drop_duplicates(subset=['PDQ']).sort_values(by='PDQ', ascending=False)
-
-    fig = px.choropleth_mapbox(combined_table, geojson=geojson, 
-                           color = "crime_count",
-                           locations="PDQ", featureidkey="properties.PDQ",
-                           center={'lat': 45.508888, 'lon': -73.561668},
-                           mapbox_style="carto-positron",
-                           zoom=9
-                           )
-    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-    return fig
+    return maps[dateindex]
 
 
 if __name__ == "__main__":
